682d748737d9b385a715a821e0f6b445d4fe335a
artemkv <artemkv@gmail.com>
2019-03-15 19:59:59 +0100

    Serialize enums as numbers, make sure values are stable

--- src/main/java/net/artemkv/referencewatches/persistence/model/CaseMaterial.java
+++ src/main/java/net/artemkv/referencewatches/persistence/model/CaseMaterial.java
@@ -1,5 +1,13 @@
 package net.artemkv.referencewatches.persistence.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.NUMBER)
 public enum CaseMaterial {
-    STAINLESS_STEEL, BRONZE, TITANIUM, GOLD, BRASS
+    STAINLESS_STEEL(0), BRONZE(1), TITANIUM(2), GOLD(3), BRASS(4);
+
+    private final int value;
+    CaseMaterial(int value) {
+        this.value = value;
+    }
 }
--- src/main/java/net/artemkv/referencewatches/persistence/model/Gender.java
+++ src/main/java/net/artemkv/referencewatches/persistence/model/Gender.java
@@ -1,5 +1,13 @@
 package net.artemkv.referencewatches.persistence.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+@JsonFormat(shape = JsonFormat.Shape.NUMBER)
 public enum Gender {
-    UNISEX, MENS, LADIES
+    UNISEX(0), MENS(1), LADIES(2);
+
+    private final int value;
+    Gender(int value) {
+        this.value = value;
+    }
 }
